a1f3ff7e2158965716a9062ce4c8bf53b40d568e
Jack Belnik <jw.belnik@gmail.com>
2023-07-31 17:36:53 +0300

    feat: add for and result all done task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function reverse (n) {
   let string = String(Math.abs(n)).split('');
   let result = [];
-
+  for (let i = string.length; i >= 0; i--) {
+    result.push(string[i]);
+  }
+  return result.join('');
 }
